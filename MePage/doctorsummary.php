b796ddc0a370f385fb4f55cfe67aa5f5988f0245
NgenoMark <114102083+NgenoMark@users.noreply.github.com>
2023-06-17 11:53:05 +0300

    Creating a database access to doctor summary

---
+++ MePage/doctorsummary.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+  <style>
+    table, th, td {
+      border: 1px solid black;
+    }
+    th, td {
+      background-color: #96D4D6;
+      border-style: solid;
+    }
+  </style>
+<body>
+  <h1>DISPLAY OF THE INSERTED DATA</h1>
+
+  <?php
+  session_start();
+  require_once("dbconnection.php");
+
+  // Posts items in the database 
+
+  $PatientSSN = isset($_POST["ssn"]) ? $_POST["ssn"] : "";
+  $FirstName = isset($_POST["fname"]) ? $_POST["fname"] : "";
+  $LastName = isset($_POST["lname"]) ? $_POST["lname"] : "";
+  $PatientIllness = isset($_POST["Illness"]) ? $_POST["Illness"] : "";
+  $PatientPrescription = isset($_POST["Prescription"]) ? $_POST["Prescription"] : "";
+  $DrugsPrescribed = isset($_POST["drugp"]) ? $_POST["drugp"] : "";
+
+  // Inserts the record into the database 
+
+ // Inserts the record into the database 
+$sql = "INSERT INTO doctorsummary (PatientSSN, firstname, lastname, PatientIllness, PatientPrescription, DrugsPrescribed)
+VALUES (?, ?, ?, ?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssss", $PatientSSN, $FirstName, $LastName, $PatientIllness, $PatientPrescription, $DrugsPrescribed);
+
+if ($stmt->execute()) {
+echo "Record inserted successfully";
+} else {
+echo "Could not insert record: " . $stmt->error;
+}
+
+
+  // Prepare the SQL statement
+  $stmt = $conn->prepare("SELECT * FROM doctorsummary WHERE PatientSSN = ? AND firstname = ? AND lastname = ? AND PatientIllness = ?  AND PatientPrescription = ? AND DrugsPrescribed = ? ");
+  $stmt->bind_param("ssssss", $PatientSSN, $FirstName, $LastName, $PatientIllness, $PatientPrescription, $DrugsPrescribed);
+  
+  // Execute the prepared statement
+  $stmt->execute();
+
+  // Get the result
+  $result = $stmt->get_result();
+
+  if ($result) {
+      if ($result->num_rows > 0) {
+          // Generate the table
+          echo '<table style="width:100%">
+              <tr>
+                  <th>Patient SSN</th>
+                  <th>First name</th>
+                  <th>Last name</th>
+                  <th>Patient Illness</th>
+                  <th style="width:400%">Patients Prescription</th>
+                  <th>Drug Prescribed</th>
+              </tr>';
+
+          while ($row = $result->fetch_assoc()) {
+              echo "<tr>
+                      <td>".$row['PatientSSN']."</td>
+                      <td>".$row['firstname']."</td>
+                      <td>".$row['lastname']."</td>
+                      <td>".$row['PatientIllness']."</td>
+                      <td>".$row['PatientPrescription']."</td>
+                      <td>".$row['DrugsPrescribed']."</td>
+                  </tr>";
+          }
+
+          echo '</table>';
+      } else {
+          echo "No records found.";
+      }
+  } else {
+      echo "Error retrieving records: " . $stmt->error;
+  }
+
+  // Close the prepared statement and database connection
+  $stmt->close();
+  $conn->close();
+  ?>
+</body>
+</html>
