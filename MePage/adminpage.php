d8f56d509289069110de7089fb1644f0d17c2de5
Ngeno Mark <kipchumbamark48@gmail.com>
2023-10-02 10:58:41 +0300

    New button and editing admin name

--- MePage/adminpage.php
+++ MePage/adminpage.php
@@ -30,32 +30,39 @@
 
         .welcome-container {
             display: flex;
-            justify-content: flex-end;
-            align-items: center;
+           
             margin: 20px;
             color: white;
         }
 
         .welcome-container h1 {
-            margin-left: 20px;
+            margin-right: 220px;
         }
     </style>
 </head>
 
 <body>
+
+<?php 
+    include("header.php")
+?>
     <?php
+
     session_start();
+    
+
+
     if (isset($_SESSION['username'])) {
         $username = $_SESSION['username'];
         echo "<div class='welcome-container'><h1>Welcome, $username</h1></div>";
     }
     ?>
 
-    <button class="btn" onclick="redirectToPatients()"> View Patients</button> <br>
+    <button class="btn" onclick="redirectToPatients()"> View Patients</button> 
     <button class="btn" onclick="redirectToInventory()"> Check Inventory </button> <br>
-    <button class="btn" onclick="redirectToUsers()"> View all users </button> <br>
-    <button class = "btn"  onclick="redirectToHistory()"> View History of Dispensing</button><br><br><br><br><br><br><br><br><br><br>
-    <button  class = "btn" onclick = "redirectToLogout()"> Logout </button>
+    <button class="btn" onclick="redirectToUsers()"> View all users </button> 
+    <button class = "btn"  onclick="redirectToHistory()"> View History of Dispensing</button>
+    <button class = "btn" onclick = "redirecttoDrugDetails()"> View Drug Details </button><br><br><br><br><br><br><br><br><br><br>
 
 
     <script>
@@ -71,12 +78,15 @@
             window.location.href = ("users.php");
         }
 
-        function redirectToHistory()
-{
-    window.location.href=("historyfetch.php")
-}   
+        function redirectToHistory(){
+            window.location.href=("historyfetch.php")
+        }
+        
+        function redirecttoDrugDetails(){
+            window.location.href = "drugdisplay.php"
+        }
 
-function redirectToLogout(){
+        function redirectToLogout(){
             window.location.href = "logoutpage.html";
         }
 
